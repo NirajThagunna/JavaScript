2f85f95dd8190d97407dcf2f419aabba45085d89
Niraj Thagunna <nirajthagunna13@gmail.com>
2024-08-15 19:45:50 +0545

    data-type-and-variable

---
+++ Fundamentals/js/script.js
@@ -0,0 +1,71 @@
+// Printing JavaScript values in browser console log
+console.log("Niraj");
+console.log(23);
+
+// Single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+////////////////////////////////////
+// Variables in JavaScript
+////////////////////////////////////
+
+// Declaring JavaScript variables
+let firstName = "Niraj Thagunna";
+console.log(firstName);
+
+// assigning a string value to a variable
+firstName = "Elon";
+console.log(firstName);
+
+// Naming convention for varibale name by using camelCase notation
+let $rupee = 100;
+console.log($rupee);
+
+let _num = 100;
+console.log(_num);
+
+let first_name = "Niraj";
+console.log(first_name);
+
+// varibales that contains all upper case that indicates Constant that will never changed
+let PI = 3.14;
+console.log(PI);
+
+//////////////////////////////////
+// Data Types in JavaScript
+//////////////////////////////////
+
+// Boolean
+true;
+console.log(true);
+
+let isEmpty = true;
+console.log(isEmpty);
+
+// typeof opeartor
+console.log(typeof isEmpty);
+console.log(typeof 23);
+console.log(typeof "Niraj Thagunna");
+
+// reassigning a value of any type to a variable -> Dynamic Typing
+isEmpty = "YES!";
+console.log(typeof isEmpty);
+console.log(isEmpty);
+
+// undefined data type - when we declare an empty variable without assigning any value
+let year; // automatically store undefined value
+console.log(year);
+console.log(typeof year);
+
+// reassigning a new value
+year = 2024;
+console.log(year);
+console.log(typeof year);
+
+// null data type
+console.log(typeof null);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JavaScript
+
+## Learning JavaScript by developing projects using HTML5 and CSS3
+
+## Fundamentals of JavaScript
+
+- Data Types
+  ↓
+- Variables and Comments
